5e1359268ebacf9fb41026f3e1d29bacc15be085
gzeon <im@gzeon.dev>
2023-07-21 15:52:03 +0200

    feat: inherit in counter example

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "./ProveOfHack.sol";
+
+contract Counter is PausableUpgradeable, ProveOfHack {
     uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    uint256 private snapshot;
+
+    function _preHackSnapshot() internal override {
+        snapshot = number;
     }
 
-    function increment() public {
-        number++;
+    function _postHackCheck() internal view override returns (bool) {
+        return number < snapshot;
+    }
+
+    function _postHackAction() internal override {
+        _pause();
+    }
+
+    function increment(uint256 x) public {
+        unchecked {
+            number += x;
+        }
     }
 }
