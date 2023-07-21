a0829d53bc407224d1d93eb5c6a9cc00956200d5
gzeon <im@gzeon.dev>
2023-07-21 16:15:08 +0200

    chore: rename to UpOnly

--- src/UpOnly.sol
+++ src/UpOnly.sol
@@ -4,20 +4,26 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "./ProveOfHack.sol";
 
-contract Counter is PausableUpgradeable, ProveOfHack {
+contract UpOnly is PausableUpgradeable, ProveOfHack {
     uint256 public number;
 
     uint256 private snapshot;
 
     function _preHackSnapshot() internal override {
+        // this will be called before invoking ProveOfHack
+        // we store the current state of the contract in a snapshot variable
         snapshot = number;
     }
 
     function _postHackCheck() internal view override returns (bool) {
+        // this will be called after invoking ProveOfHack to check if there is a hack
+        // we define the hack as the number decremented in this example
         return number < snapshot;
     }
 
     function _postHackAction() internal override {
+        // this will be called when ProveOfHack identified a hack
+        // we pause the contract in this example
         _pause();
     }
 
--- test/UpOnly.sol
+++ test/UpOnly.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../src/UpOnly.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract UpOnlyTest is Test {
+    UpOnly public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new UpOnly();
     }
 
     function testIncrement() public {
